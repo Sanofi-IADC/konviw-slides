9512f318a89cc6fb6daad42e882a7e02f0b2cfaa
Krystian Kedra <krystian.kedra-ext@sanofi.com>
2022-10-23 12:36:58 +0200

    feat: WEB-202 Test routes

--- app.js
+++ app.js
@@ -4,65 +4,51 @@ import compression from 'compression';
 import cookieParser from 'cookie-parser';
 import errorHandler from 'errorhandler';
 import morgan from 'morgan';
-
-// atlassian-connect-express also provides a middleware
 import ace from 'atlassian-connect-express';
-
-// Use Handlebars as view engine:
-// https://npmjs.org/package/express-hbs
-// http://handlebarsjs.com
 import hbs from 'express-hbs';
-
-// We also need a few stock Node modules
 import http from 'http';
 import path from 'path';
-import os from 'os';
+import * as sqlite3 from 'sqlite3';
 import helmet from 'helmet';
 import nocache from 'nocache';
-
-// Routes live here; this is the C in MVC
 import routes from './routes';
 import { addServerSideRendering } from './server-side-rendering';
 
 const app = express();
 
-const addon = ace(app, { config: {
-  production: {
-    environment: 'production',
-    port: process.env.PORT,
-    store: {
-      adapter: "sequelize",
-      dialect: "sqlite3",
-      logging: false,
-      type: "memory"
-    },
-    errorTemplate: true,
-    localBaseUrl: ".",
-    product: "confluence",
+const addon = ace(app, {
+  config: {
+    production: {
+      environment: 'production',
+      port: process.env.PORT,
+      store: {
+        adapter: 'sequelize',
+        dialect: 'sqlite3',
+        logging: false,
+        type: 'memory'
+      },
+      errorTemplate: true,
+      localBaseUrl: '.',
+      product: 'confluence',
+    }
   }
-} });
+});
 
-// See config.json
+
 const port = addon.config.port();
 app.set('port', port);
 
-// Log requests, using an appropriate formatter by env
 const devEnv = app.get('env') === 'development';
 app.use(morgan(devEnv ? 'dev' : 'combined'));
 
-// Configure Handlebars
 const viewsDir = path.join(__dirname, 'views');
 const handlebarsEngine = hbs.express4({partialsDir: viewsDir});
 app.engine('hbs', handlebarsEngine);
 app.set('view engine', 'hbs');
 app.set('views', viewsDir);
 
-// Configure jsx (jsx files should go in views/ and export the root component as the default export)
 addServerSideRendering(app, handlebarsEngine);
 
-// Atlassian security policy requirements
-// http://go.atlassian.com/security-requirements-for-cloud-apps
-// HSTS must be enabled with a minimum age of at least one year
 app.use(helmet.hsts({
   maxAge: 31536000,
   includeSubDomains: false
@@ -71,35 +57,22 @@ app.use(helmet.referrerPolicy({
   policy: ['origin']
 }));
 
-// Include request parsers
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-
-// Gzip responses when appropriate
 app.use(compression());
-
-// Include atlassian-connect-express middleware
 app.use(addon.middleware());
 
-// Mount the static files directory
 const staticDir = path.join(__dirname, 'public');
 app.use(express.static(staticDir));
 
-// Atlassian security policy requirements
-// http://go.atlassian.com/security-requirements-for-cloud-apps
 app.use(nocache());
 
-// Show nicer errors in dev mode
 if (devEnv) app.use(errorHandler());
 
-// Wire up routes
 routes(app, addon);
 
 // Boot the HTTP server
 http.createServer(app).listen(port, () => {
-  console.log('App server running at http://' + os.hostname() + ':' + port);
-
-  // Enables auto registration/de-registration of app into a host in dev mode
   if (devEnv) addon.register();
 });
