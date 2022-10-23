376a1c24b501b4f22772534a31e184df9f48b4ed
Krystian Kedra <krystian.kedra-ext@sanofi.com>
2022-10-23 11:40:30 +0200

    feat: WEB-202 Test routes

--- routes/index.js
+++ routes/index.js
@@ -50,7 +50,8 @@ export default function routes(app, addon) {
   // Redirect root path to /atlassian-connect.json,
   // which will be served by atlassian-connect-express.
   app.get('/', (req, res) => {
-    res.redirect('/atlassian-connect.json')
+    const loadJSON = (path) => JSON.parse(fs.readFileSync(new URL(path, import.meta.url)));
+    res.redirect(loadJSON('/atlassian-connect.json'))
   })
 
   // app.get('/slide', addon.authenticate(),function (req, res) {
