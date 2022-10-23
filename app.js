a689f726e88b224a73d109fe4f18e5790bd2aa89
Krystian Kedra <krystian.kedra-ext@sanofi.com>
2022-10-23 11:33:11 +0200

    feat: WEB-202 Test routes

--- app.js
+++ app.js
@@ -93,5 +93,5 @@ http.createServer(app).listen(port, () => {
   console.log('App server running at http://' + os.hostname() + ':' + port);
 
   // Enables auto registration/de-registration of app into a host in dev mode
-  if (devEnv) addon.register();
+  // if (devEnv) addon.register();
 });
