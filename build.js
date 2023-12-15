7d3a219650e14b7dafa45ec5e265bd34d5740635
Krystian Kedra <krystian.kedra-ext@sanofi.com>
2023-12-15 12:06:10 +0100

    feat: WEB-2111 Return empty object JSON for vercel deployment

--- build.js
+++ build.js
@@ -5,10 +5,10 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-config.version = pkg.version
-config.baseUrl = process.env.APP_BASE_URL
-config.links.self = process.env.APP_BASE_URL + '/atlassian-connect.json'
-config.links.homepage = process.env.APP_BASE_URL + '/atlassian-connect.json'
+// config.version = pkg.version
+// config.baseUrl = process.env.APP_BASE_URL
+// config.links.self = process.env.APP_BASE_URL + '/atlassian-connect.json'
+// config.links.homepage = process.env.APP_BASE_URL + '/atlassian-connect.json'
 const writeStream = fs.createWriteStream('atlassian-connect.json')
 writeStream.write(JSON.stringify(config))
 writeStream.end()
